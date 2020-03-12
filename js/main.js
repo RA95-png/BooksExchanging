bc9e3010dac3fe45e785baa7987f054938067047
Eman-El-Zetawy <eman.el.zetawy12@gmail.com>
2020-03-12 22:54:03 +0200

    3/12

--- js/main.js
+++ js/main.js
@@ -189,12 +189,12 @@
         max: maxPrice,
 		values: [minPrice, maxPrice],
 		slide: function (event, ui) {
-			minamount.val('$' + ui.values[0]);
-			maxamount.val('$' + ui.values[1]);
+			minamount.val( ui.values[0]+'JOD' );
+			maxamount.val(ui.values[1] + 'JOD');
 		}
 	});
-	minamount.val('$' + rangeSlider.slider("values", 0));
-    maxamount.val('$' + rangeSlider.slider("values", 1));
+	minamount.val( rangeSlider.slider("values", 0)+'JOD' );
+    maxamount.val( rangeSlider.slider("values", 1)+'JOD' );
 
     /*-------------------
 		Radio Btn
